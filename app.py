7d8335b28ec808984b1bd26365d980432b24286f
arispretz <arispretz@gmail.com>
2024-04-12 05:13:14 -0300

    one change

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongodb_uri = env("MONGODB_URI")
 client = pymongo.MongoClient(mongodb_uri)
 
 # Creating database
-db = client.text_analyser
+db = client.text_analyzer
 # Creating students collection and inserting a document
 db.friends.insert_one({'name': 'Ariana', 'country': 'Argentina', 'city': 'San Lorenzo', 'age': 44})
 print(client.list_database_names())
@@ -34,26 +34,26 @@ friends = [
 for friend in friends:
     db.friends.insert_one(friend)
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 friends = db.friends.find_one()
 print(friend)
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 friend = db.friends.find_one({'_id': ObjectId('5df68a23f106fe2d315bbc8c')})
 print(friend)
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 friends = db.friends.find()
 for friend in friends:
     print(friend)
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 friends = db.friends.find({}, {"_id":0,  "name": 1, "country":1}) # 0 means not include and 1 means include
 for friend in friends:
     print(friend)
 
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 query = {
     "country":"Finland"
 }
@@ -62,7 +62,7 @@ friends = db.friends.find(query)
 for friend in friends:
     print(friend)   
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 query = {
     "city":"Helsinki"
 }
@@ -70,7 +70,7 @@ friends = db.friends.find(query)
 for friend in friends:
     print(friend)
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 query = {
     "country":"Finland",
     "city":"Helsinki"
@@ -79,22 +79,22 @@ friends = db.friends.find(query)
 for friend in friends:
     print(friend)
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 query = {"age":{"$gt":30}}
 friends = db.friends.find(query)
 for friend in friends:
     print(friend)
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 query = {"age":{"$gt":30}}
 friends = db.friends.find(query)
 for friend in friends:
     print(friend)
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 db.friends.find().limit(3)
 
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 friends = db.friends.find().sort('name')
 for friend in friends:
     print(friend)
@@ -111,7 +111,7 @@ friends = db.friends.find().sort('age',-1)
 for friend in friends:
     print(friend)
     
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 
 query = {'age':44}
 new_value = {'$set':{'age':45}}
@@ -122,7 +122,7 @@ for friend in db.friends.find():
     print(friend)
     
 # When we want to update many documents at once we use update_many() method.
-db = client['text_analyser'] # accessing the database
+db = client['text_analyzer'] # accessing the database
 
 query = {'name':'John'}
 db.friends.delete_one(query)
@@ -133,7 +133,7 @@ for friend in db.friends.find():
 for friend in db.friends.find():
     print(friend)
     
-'''db = client['text_analyser'] # accessing the database
+'''db = client['text_analyzer'] # accessing the database
 db.friends.drop()'''
 
 
@@ -231,65 +231,3 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
     
-    
-    '''
-import re
-
-def limpiar_texto(texto):
-    # Convertir todas las letras a minúsculas
-    texto = texto.lower()
-    # Eliminar caracteres no alfabéticos, números y espacios adicionales
-    texto = re.sub(r'[^a-záéíóúüñ\s]', '', texto)
-    # Eliminar espacios adicionales
-    texto = re.sub(r'\s+', ' ', texto)
-    return texto.strip()  # Eliminar espacios al principio y al final
-
-texto = "Este es un ejemplo de texto con 123 números y signos de puntuación!!!"
-texto_limpo = limpiar_texto(texto)
-print("Texto limpio:", texto_limpo)
-
-
-    import re
-
-def contar_palabras_regex(texto):
-    palabras = re.findall(r'\w+', texto)
-    return len(palabras)
-
-texto = "Este es un ejemplo de texto para contar palabras en Python"
-resultado = contar_palabras_regex(texto)
-print("Cantidad de palabras:", resultado)
-
-
-def contar_caracteres(texto):
-    return len(texto)
-
-texto = "Este es un ejemplo de texto para contar caracteres en Python"
-resultado = contar_caracteres(texto)
-print("Cantidad de caracteres:", resultado)
-
-
-from collections import Counter
-import re
-
-def palabras_mas_frecuentes(texto, cantidad=5):
-    palabras = re.findall(r'\w+', texto.lower())
-    conteo = Counter(palabras)
-    return conteo.most_common(cantidad)
-
-texto = """
-Este es un ejemplo de texto. Este texto tiene varias palabras, algunas de las cuales se repiten más que otras. 
-Por ejemplo, la palabra "texto" se repite varias veces en este texto de ejemplo.
-"""
-
-resultado = palabras_mas_frecuentes(texto)
-print("Palabras más frecuentes:")
-for palabra, frecuencia in resultado:
-    print(f"{palabra}: {frecuencia} veces")
-
-
-
-    '''
-    
-    '''
-    quiero construir una app simple para analizar textos. debo desarrollarla con python, flask, javaScript, desplegada en render.com y estos son los archivos que tengo hasta el momento pero la app no devuelve el texto con sus correcciones. que es lo que sucede:
-    '''
